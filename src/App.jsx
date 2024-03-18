15746ea76008362451c119f8e2753f78a6edcd51
NicolaeGilca <gilcaniku@gmail.com>
2024-03-18 08:53:00 +0200

    start app

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { ChatControls } from './components/chat-controls'
+import { ChatDiscussionList } from './components/chat-discussion-list'
+import { ChatLayout } from './components/chat-layout'
+import { ChatMessage } from './components/chat-message'
+import { ChatStartDiscussionModal } from './components/chat-start-discussion-modal'
+
+
+function Button(props) {
+}
+
+function App() {
+
+return <>
+        {false && <ChatStartDiscussionModal/>}
+        <ChatLayout 
+        controls={<ChatControls/>}
+        aside={<ChatDiscussionList/>}
+        main={<ChatMessage/>}
+        />
+
+      </>
+ 
+}
+
+export default App
